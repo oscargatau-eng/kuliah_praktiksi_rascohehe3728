2374935022fe46c9275ac0cc36579bd971bdaaf2
oscargatau-eng <oscargatau@gmail.com>
2026-05-23 09:36:33 +0700

    Create app.py

---
+++ app.py
@@ -0,0 +1,12 @@
+Import streamlit as st
+
+st.set_page_config(
+  page_title="Finance Dashboard",
+  layout="wide"
+)
+
+# Hirarki teks
+st.title("📊 Dashboard")
+st.header("Laporan Bulanan")
+st.subheader("📈 Monthly Expenses")
+st.caption("Made with ❤️ using Streamlit")
